c6f59353a846b631c02f971442718646a04013a5
alexciccarese <alex.ciccarese@gmail.com>
2025-03-10 15:41:03 +0100

    add people function

--- people.js
+++ people.js
@@ -1,2 +1,16 @@
 const person = require('./name')
 const hobby = require('./hobbies')
+
+function personProfile() {
+  const fullName = person('Lucia', 'Verdi')
+  const hobbies = hobby('Cucina', 'Tecnologia', 'Pallavolo')
+
+
+  return {
+    fullName,
+    hobbies
+  }
+}
+
+const result = personProfile()
+console.log(result);
